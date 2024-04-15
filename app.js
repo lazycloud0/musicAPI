21fc1312ae162a4411105f81e3099344fe78b823
W <lazypink@gmail.com>
2024-04-15 22:29:52 +0100

    added security measures

--- app.js
+++ app.js
@@ -1,145 +1,145 @@
 // importing express
 // importing data
+// importing helmet
 import express from "express";
-import music from "./music_data.js"
+import music from "./music_data.js";
+import helmet from "helmet";
 
-// Calling express functions 
+// Calling express functions
 const app = express();
 app.use(express.json());
+// use Helmet
+app.use(helmet());
 
+let music_ds = music;
 
 // Create CRUD requests
 
-
 // GET request test
 app.get("/", (req, res) => {
-    res.json({message: "Hello from root path!" })
-})
+  res.json({ message: "Hello from root path!" });
+});
 
 // GET request trivia
 app.get("/music", (req, res) => {
-    res.status(200).json({success:true, payload: music})
-  })
+  res.status(200).json({ success: true, payload: music_ds });
+});
 
-// GET request by ID 
+// GET request by ID
 app.get("/music/:id", (req, res) => {
   try {
-    const reqID = Number(req.params.id)
+    const reqID = Number(req.params.id);
 
-    function matchID(music) {
-      return music.id === reqID
+    function matchID(music_ds) {
+      return music_ds.id === reqID;
     }
-    const track = music.find(matchID)
+    const track = music_ds.find(matchID);
 
-    if(!track) {
-      res.json({"success": false, "payload": {message: "track not found"}})
+    if (!track) {
+      res.json({ success: false, payload: { message: "track not found" } });
     } else {
-      res.json({"success":true, "payload": track})
+      res.json({ success: true, payload: track });
     }
   } catch (err) {
-    res.json({"success": false, "payload": {message: "error"}})
+    res.json({ success: false, payload: { message: "error" } });
   }
-})
+});
 
-//POST request 
+//POST request
 app.post("/music", (req, res) => {
-  try{
+  try {
     // const id = music.length;
     // const artist = req.body.artist
     // const title = req.body.title
     // const album = req.body.album
     // const duration = req.body.duration
     // const year_release = req.body.year_release
-  
-    const track = req.body
-    track.id = music.length
+
+    const track = req.body;
+    track.id = music_ds.length;
     music.push(track);
-    
-    res.status(200).json({success: true, payload: track})
 
+    res.status(200).json({ success: true, payload: track });
   } catch (err) {
-    res.json({"success": false, "payload": {message: "error"}})
+    res.json({ success: false, payload: { message: "error" } });
   }
-  })
+});
 
-// PUT request by ID 
+// PUT request by ID
 app.put("/music/:id", (req, res) => {
-  try{
-    const reqID = Number(req.params.id)
+  try {
+    const reqID = Number(req.params.id);
 
-    function matchID(music) {
-      return music.id === reqID
-    }
-    music[matchID] = req.body
-    
-    res.status(200).json({success: true, payload: music[matchID]})
+    // function matchID(music) {
+    //   return music.id === reqID
+    // }
+    music_ds[reqID] = req.body;
 
+    res.status(200).json({ success: true, payload: music_ds[reqID] });
   } catch (err) {
-    res.json({"success": false, "payload": {message: "error"}})
+    res.json({ success: false, payload: { message: "error" } });
   }
-  })
+});
 
-  
 // PATCH request by ID
-app.patch("/questions/:id", (req, res) => {
-    const reqID = Number(req.params.id)
-    const artist = req.body.artist
-    const title = req.body.title
-    const album = req.body.album
-    const duration = req.body.durationy
-    const year_release = req.body.year_release
+app.patch("/music_ds/:id", (req, res) => {
+  const reqID = Number(req.params.id);
+  const artist = req.body.artist;
+  const title = req.body.title;
+  const album = req.body.album;
+  const duration = req.body.durationy;
+  const year_release = req.body.year_release;
 
-    //create a function that finds the index and compares it to the id selected
-    const found = questions.findIndex((question) => {
-      return question.id === id;
-    });
-  
-    //create if statements that check whether the the id exists
-    if (found) {
-      if (artist) {
-        questions[found].artist = artist;
-      }
-      if (title) {
-        questions[found].title = title;
-      }
-      if (album) {
-        questions[found].album = album;
-      }
-      if (duration) {
-        questions[found].duration = duration;
-      }
-      if (year_release) {
-        questions[found].year_release = year_release;
-      }
-
-    }
-    //return a response for pass and fail
-    res.status(200).json({
-      success: true,
-      payload: questions[found],
-    });
-    res.status(404).json({ success: false, message: "Failed to update" });
+  //create a function that finds the index and compares it to the id selected
+  const found = music.findIndex((music_ds) => {
+    return music_ds.id === id;
   });
 
-
+  //create if statements that check whether the the id exists
+  if (found) {
+    if (artist) {
+      music_ds[found].artist = artist;
+    }
+    if (title) {
+      music_ds[found].title = title;
+    }
+    if (album) {
+      music_ds[found].album = album;
+    }
+    if (duration) {
+      music_ds[found].duration = duration;
+    }
+    if (year_release) {
+      music_ds[found].year_release = year_release;
+    }
+  }
+  //return a response for pass and fail
+  res.status(200).json({
+    success: true,
+    payload: music_ds[found],
+  });
+  res.status(404).json({ success: false, message: "Failed to update" });
+});
 
 // DELETE request by ID
 app.delete("/music/:id", (req, res) => {
   try {
-    const reqID = Number(req.params.id)
-    console.log(reqID)
+    const reqID = Number(req.params.id);
+    console.log(reqID);
     function matchID(music) {
-      return music.id === reqID
+      return music.id === reqID;
     }
-    const track = music.find(matchID)
-    console.log(track)
+    const track = music.find(matchID);
+    console.log(track);
     //music = music.splice(0,reqID) + music.splice(reqID+1, -1)
-    delete music[reqID]
-    res.status(200).json({success: true, payload: music})
+    //delete music[reqID]
+    music[reqID] = {};
+    res.status(200).json({ success: true, payload: music });
   } catch (err) {
-    res.json({"success": false, "payload": {message: "error"}})
+    res.json({ success: false, payload: { message: "error" } });
   }
-})
+});
 
 
-export default app
+
+export default app;
--- index.js
+++ index.js
@@ -5,6 +5,20 @@ import app from "./app.js"
 const PORT = 3000
 
 
+// reduce fingerprinting
+app.disable('x-powered-by')
+
+// custom 404
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+  })
+  
+  // custom error handler
+  app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
+  
 // listen request
 app.listen(PORT, ()=>{
     console.log(`App listening at http://localhost:${PORT}`)
