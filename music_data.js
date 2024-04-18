7a6169867f62d0e077dc19c087fc22f5a40ae068
W <lazypink@gmail.com>
2024-04-18 11:03:49 +0100

    husky added

--- music_data.js
+++ music_data.js
@@ -1,44 +1,52 @@
 const music = [
-    {
-        id: 0,
-        artist: "Damon Albarn",
-        title: "Mr Tembo",
-        album: "Everyday Robots",
-        duration: "3:43",
-        year_release: "2014"
-    },
-    {
-        id: 1,
-        artist: "Arctic Monkeys",
-        title: "Star Treatment",
-        album: "Tranquility Base Hotel & Casino",
-        duration: "5:55",
-        year_release: "2018"
-    },
-    {
-        id: 2,
-        artist: "King Krule",
-        title: "Biscuit Town",
-        album: "The Ooz",
-        duration: "3:42",
-        year_release: "2017"
-    },
-    {
-        id: 3,
-        artist: "Sufjan Stevens",
-        title: "Mystery of Love",
-        album: "OST Call Me By Your Name",
-        duration: "4.09",
-        year_release: "2018"
-    },
-    {
-        id: 4,
-        artist: "Noughts and Exes",
-        title: "Hearts",
-        album: "Noughts and Exes",
-        duration: "4:26",
-        year_release: "2014"
-    },
-]
+  {
+    id: 0,
+    artist: "Damon Albarn",
+    title: "Mr Tembo",
+    album: "Everyday Robots",
+    duration: "3:43",
+    year_release: "2014",
+  },
+  {
+    id: 1,
+    artist: "Arctic Monkeys",
+    title: "Star Treatment",
+    album: "Tranquility Base Hotel & Casino",
+    duration: "5:55",
+    year_release: "2018",
+  },
+  {
+    id: 2,
+    artist: "King Krule",
+    title: "Biscuit Town",
+    album: "The Ooz",
+    duration: "3:42",
+    year_release: "2017",
+  },
+  {
+    id: 3,
+    artist: "Sufjan Stevens",
+    title: "Mystery of Love",
+    album: "OST Call Me By Your Name",
+    duration: "4.09",
+    year_release: "2018",
+  },
+  {
+    id: 4,
+    artist: "Noughts and Exes",
+    title: "Hearts",
+    album: "Noughts and Exes",
+    duration: "4:26",
+    year_release: "2014",
+  },
+  {
+    id: 5,
+    artist: "test",
+    title: "test",
+    album: "test",
+    duration: "4:26",
+    year_release: "2014",
+  },
+];
 
-export default music
+export default music;
